71e5aec9721dbc3595155b0310521cffcb2748f8
Damian Oguche <damianguch@yahoo.com>
2024-06-03 09:56:16 +0000

    modified src/App.js to separate the /reducer route from the home page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@
  * REACT STATE MANAGEMENT
  * 1. Using Redux(Redux Tool Kit)
  */
-
-import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -13,21 +12,36 @@ import Calculator from './components/Calculator';
 import FeedbackForm from './components/FeedbackForm';
 import GoalApp from './components/GoalApp';
 import Employees from './components/Employees';
+import NavBar from './components/NavBar';
+import Reducer from './components/Reducer';
 
 function App() {
   return (
-    <Fragment>
-      <Provider store={store}>
-        <div className="mb-5">
-          <MealsList />
-          <Counter />
-        </div>
-      </Provider>
-      <Calculator />
-      <FeedbackForm />
-      <GoalApp />
-      <Employees />
-    </Fragment>
+    <div className="">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/reducer" element={<Reducer />} />
+          <Route
+            path="/"
+            element={
+              <div className="mb-5">
+                <Provider store={store}>
+                  <div className="mb-5">
+                    <MealsList />
+                    <Counter />
+                  </div>
+                </Provider>
+                <Calculator />
+                <FeedbackForm />
+                <GoalApp />
+                <Employees />
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
